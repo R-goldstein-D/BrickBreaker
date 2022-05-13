35527eda9f01f41b4b8c10e96d4905426e9865d0
Charles Sarichith <charsari337@ed.amdsb.ca>
2022-05-13 09:39:07 -0400

    ui ui

--- BrickBreaker/InstructionScreen.cs
+++ BrickBreaker/InstructionScreen.cs
@@ -15,6 +15,32 @@ namespace BrickBreaker
         public InstructionScreen()
         {
             InitializeComponent();
+
+            instructionLabel.Text = "Instructions";
+
+            instructionLabel.ForeColor = Color.Yellow;
+
+            instructionLabel.Text += "\n\nControls";
+            instructionLabel.Text += "\nUse Arrow Keys to move the paddle left or right.";
+            instructionLabel.Text += "\n\nGoal";
+            instructionLabel.Text += "\nYour goal is to break all bricks on screen by bouncing " +
+                "the ball off of the paddle to hit the bricks.";
+            instructionLabel.Text += "\n\nPower Ups";
+            instructionLabel.Text += "\nWhen a brick is broken there is a chance for a power up to spawn.";
+            instructionLabel.BackColor = Color.Transparent;
+
+            returnButton.Text = "Return";
+        }
+
+        private void returnButton_Click(object sender, EventArgs e)
+        {
+            MenuScreen ms = new MenuScreen();
+            Form form = this.FindForm();
+
+            form.Controls.Add(ms);
+            form.Controls.Remove(this);
+
+            ms.Location = new Point((form.Width - ms.Width) / 2, (form.Height - ms.Height) / 2);
         }
     }
 }
